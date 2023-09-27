30b57a2fc282c10a3314751451f498fb1c7eaa3f
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2023-09-27 22:12:12 +0530

    Add product image

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Description goes here'
+            description: 'Description goes here',
+            image: './assets/images/socks_green.jpg',
         };
     }
 });
